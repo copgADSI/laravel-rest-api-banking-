d1aab3896a09f6b5dab647e9f78fdc354c6c7370
Cristian <CristianGualteros23@gmail.com>
2021-08-05 10:19:51 -0500

    Methods store n deposit

--- app/Http/Controllers/BalanceController.php
+++ app/Http/Controllers/BalanceController.php
@@ -11,6 +11,32 @@ class BalanceController extends Controller
     {
     }
 
+    public function store(Request $request)
+    {
+        if ($request->input('type') === 'deposit') {
+            return $this->deposit(
+                $request->input('destination'),
+                $request->input('amount')
+            );
+        }
+    }
+
+    private function deposit($destination, $amount)
+    {
+
+        $account = Account::firstOrCreate([
+            'id' => $destination
+        ]);
+        $account->balance += $amount;
+        $account->save(); //UPDATE
+
+        return response()->json([
+            'destination' => [
+                'id' => $account->id,
+                'balance' =>  $account->balance,
+            ]
+        ]);
+    }
     public function show(Request $request)
     {
         $account_id = $request->input('account_id');
